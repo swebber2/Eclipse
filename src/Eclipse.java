f70419f9765c94eebcffeed7b22cbe62a27bef28
Sean Webber <swebber2@carpenter-06.local>
2017-02-04 13:07:44 -0500

    Completed almost everything except determining what is static

--- src/Eclipse.java
+++ src/Eclipse.java
@@ -18,12 +18,20 @@ public class Eclipse {
 			Class theClass = Class.forName(className);
 			Field[] theFields = theClass.getFields();
 			for(int i = 0; i< theFields.length; i++){
-				System.out.println(theFields[i].getName());
+				System.out.print(theFields[i].getName()+ " : ");
+				System.out.print(theFields[i].getType()+ " - ");
+				System.out.print(theFields[i].getDeclaringClass());
 				System.out.println();
 			}
 			Method[] theMethods = theClass.getMethods();
 			for(int i=0; i<theMethods.length; i++){
-				System.out.println(theMethods[i].getName());
+				System.out.print(theMethods[i].getName()+ " ");
+				Class<?>[] parameterTypes = theMethods[i].getParameterTypes();
+				for (int j =0; j <  parameterTypes.length; j++){
+					System.out.print("(" + parameterTypes[j]+ ") ");
+				}
+				System.out.print(": " + theMethods[i].getReturnType());
+				System.out.print(" - " + theMethods[i].getDeclaringClass());
 				System.out.println();
 				
 			}
